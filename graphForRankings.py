330be6733f92288a6b76419758a2abd3a9c70e8a
Alessio Di Pasquale <alessiodipasquale2000@gmail.com>
2022-06-19 19:54:46 +0200

    Fixed Graphs for Dataset 1

--- graphForRankings.py
+++ graphForRankings.py
@@ -1,9 +1,12 @@
+import re
 from turtle import color
 import matplotlib.pyplot as plt
 import json
 import numpy as np
 import os
 import random
+import operator
+from tabulate import tabulate
 #Deprecated valuesArray.append(data['deprecated'])
 #Normal                    
 
@@ -16,41 +19,101 @@ valuesNormalArray = []
 valuesPreferredArray = []
 valuesCountArray = []
 
+
+res = []
+
 for filename in os.listdir(directory):
-    #if count < 100:
-     #   rand = random.randint(1,100)
-      #  if rand % 2 == 0:
+    #if count < 10:
+    #    rand = random.randint(1,100)
+     #   if rand % 2 == 0:
             f = os.path.join(directory, filename)
             if not filename.startswith('errors'):
                 with open(f,'r') as file:  
-                    data = json.load(file)
-                    
+                    data = json.load(file) 
                     entity = str(filename.replace('.json',''))
-                    fileNameArray.append(entity)
-                    valuesDeprecatedArray.append(data['deprecated'])
-                    valuesNormalArray.append(data['normal'])
-                    valuesPreferredArray.append(data['preferred'])
-                    valuesCountArray.append(data['count'])
+                    el = {
+                        "entity": str(filename.replace('.json','')),
+                        "deprecated" : data['deprecated'],
+                        "normal": data['normal'],
+                        "preferred": data['preferred'],
+                        "count": data['count']
+                    }                  
+                    res.append(el)
+                    #entity = str(filename.replace('.json',''))
+                    #fileNameArray.append(entity)
+                    #valuesDeprecatedArray.append(data['deprecated'])
+                    #valuesNormalArray.append(data['normal'])
+                    #valuesPreferredArray.append(data['preferred'])
+                    #valuesCountArray.append(data['count'])
 
                     count+=1
-ind = np.arange(len(fileNameArray))
-#valuesDeprecatedArray = np.
+def deprecated(el):
+    return el.get('deprecated')
+res.sort(key=deprecated, reverse=True)
+with open('sorted/deprecated.json','w') as f:
+    f.write(str(res))
+dep=[]
+names = []
+for i in range(0,100):
+    dep.append(res[i]['deprecated'])
+    names.append(res[i]['entity'])
+
+ind = np.arange(len(dep))
 plt.figure()
-plt.bar(ind, valuesDeprecatedArray, tick_label=fileNameArray, width=0.7, color='red')
+plt.bar(ind, dep, tick_label=names, width=0.9, color='red')
 plt.xticks(fontsize=4, rotation =90)
 
-#plt.figure()
-#plt.bar(ind, valuesNormalArray, tick_label=fileNameArray, width=0.7, color='orange')
-#plt.xticks(fontsize=4, rotation =90)
+def normal(el):
+    return el.get('normal')
+res.sort(key=normal, reverse=True)
+nor=[]
+names = []
+with open('sorted/normal.json','w') as f:
+    f.write(str(res))
+for i in range(0,100):
+    nor.append(res[i]['normal'])
+    names.append(res[i]['entity'])
+    
+print(tabulate({'Entity':names, 'Normal':nor}, headers="keys", tablefmt='fancy_grid'))
 
-#plt.figure()
-#plt.bar(ind, valuesPreferredArray, tick_label=fileNameArray, width=0.7, color='green')
-#plt.xticks(fontsize=4, rotation =90)
+ind = np.arange(len(nor))
+plt.figure()
+plt.bar(ind, nor, tick_label=names, width=0.9, color='orange')
+plt.xticks(fontsize=4, rotation =90)
 
-#plt.figure()
-#plt.bar(ind, valuesCountArray, tick_label=fileNameArray, width=0.7, color='purple')
-#plt.xticks(fontsize=4, rotation =90)
-#plt.show()       
+
+def preferred(el):
+    return el.get('preferred')
+res.sort(key=preferred, reverse=True)
+pre=[]
+names = []
+with open('sorted/preferred.json','w') as f:
+    f.write(str(res))
+for i in range(0,100):
+    pre.append(res[i]['preferred'])
+    names.append(res[i]['entity'])
+
+ind = np.arange(len(nor))
+plt.figure()
+plt.bar(ind, pre, tick_label=names, width=0.9, color='green')
+plt.xticks(fontsize=4, rotation =90)
+
+def count(el):
+    return el.get('count')
+res.sort(key=count, reverse=True)
+count=[]
+names = []
+with open('sorted/count.json','w') as f:
+    f.write(str(res))
+for i in range(0,100):
+    count.append(res[i]['count'])
+    names.append(res[i]['entity'])
+
+ind = np.arange(len(count))
+plt.figure()
+plt.bar(ind, count, tick_label=names, width=0.9, color='purple')
+plt.xticks(fontsize=4, rotation =90)
+plt.show()       
     
 #MEDIA    
 #mediaDeprecated = np.mean(valuesDeprecatedArray)       
